d6609adbde43e0b30dfb6058b148faf092eef6ee
David Smith <djksmith95@gmail.com>
2019-08-01 21:35:52 +0100

    optimizing and prep to get results. server takes ages

--- main.py
+++ main.py
@@ -1,19 +1,14 @@
+import os
 import geopandas as gpd
+import pandas as pd
 import rasterio as rio
-from rasterio import features
-from rasterio.merge import merge
-from rasterio.plot import show
-import pylab as pl
+import json
 from fiona.crs import from_epsg
 from shapely.geometry import box as geobox
-from shapely.geometry import shape
-import json
-import geojson
+
+import time
+
 import numpy as np
-import pandas as pd
-import os
-from glob import glob
-from subprocess import check_output
 import matplotlib.pyplot as plt
 from keras.models import Sequential
 import land_classification as lc
@@ -23,9 +18,11 @@ from keras.regularizers import l2
 from keras import optimizers
 from sklearn.model_selection import train_test_split
 from sklearn import preprocessing
-from skimage.segmentation import felzenszwalb, quickshift
 
+import tensorflow
 import segment as seg
+
+root_path = os.getcwd()
 
 print('Imports done')
 
@@ -35,16 +32,13 @@ aoi_geo = geobox(-2.29, 51.51, -1.71, 51.61)
 aoi = gpd.GeoDataFrame([], geometry=[aoi_geo])
 aoi.crs = from_epsg(4326)
 aoi.to_file('data/aoi.geojson', driver='GeoJSON')
+# aoi = 'data/aoi.geojson'
+# Getting land-cover classes
 
-# # Getting land-cover classes 
+with open(root_path + '/data/labels.json') as jf:
+    names = json.load(jf)
 
-# with open('data/labels.json') as jf:
-#     names = json.load(jf)
-    
-# root_path = check_output(['git', 'rev-parse', '--show-toplevel']).strip().decode()
-
-
-# # Reading and merging band data
+# Reading and merging band data
 
 # s2_band = 'S2A.SAFE'
 # data, profile = lc.merge_bands(s2_band, res='10')
@@ -54,10 +48,11 @@ aoi.to_file('data/aoi.geojson', driver='GeoJSON')
 # lc.write_raster('data/swindon/merged.tif', data, profile)
 # lc.mask_raster(aoi, 'data/swindon/merged.tif', 'data/swindon/masked.tif')
 
-tif = rio.open('data/swindon/masked.tif')
+tif = rio.open(root_path + '/data/masked.tif')
 
 segment_df, segments, shapes = seg.create_segment_polygon(tif)
 
+seg.plot_segments(segments)
 zones_df, dist_values = seg.get_zones_and_dists(segment_df)
 
 zones_df = seg.tag_zones(zones_df, dist_values)
@@ -68,60 +63,108 @@ segment_df = segment_df.dropna()
 
 pe = lc.PointExtractor(aoi)
 
-points_df = pe.get_n(50000)
+points_df = pe.get_n(100)
 
 bands = ['B02', 'B03', 'B04', 'B08']
 
-points_df = lc.sample_raster(points_df, 'data/Corine_S2_Proj_2.tif', bands=['labels'])
+points_df = lc.sample_raster(points_df, root_path + '/data/Corine_S2_Proj_2.tif', bands=['labels'])
+
+# ldict = {
+#         1: [(i) for i in range(1,12)],             # Artificial surfaces: 1 - 11
+#         2: [(i) for i in range(12,23)],            # Agriculture: 12 - 22
+#         3: [(i) for i in range(23,30)],            # Forest and vegetation: 23 - 30
+#         4: [(i) for i in range(30,35)],            # Open space with little veg: 30-34
+#         5: [(i) for i in range(35,45)]             # Water: 35 - 44
+#     }
+
+# names2 = {v: k for k, v in names.items()}
+# lbls = pd.Series(points_df.labels_1.values)
+
+# ent_df = []
+
+# for entry in (lbls):
+#     # for elem in entry:
+#     print((entry))
+#     new_entry = []
+#     for k,v in ldict.items():
+#         for val in v:
+#             if entry == val:
+#                 entry = k
+#     new_entry.append(entry)
+
+# ent_df.append(new_entry)
 
 points_df['segment_id'] = np.nan
 
-# points_test = points_df.iloc[0:1000,:]
-# segment_test = segment_df.iloc[0:1000,:]
-#points_df['segment_id'] = points_df.iloc[:,-1].apply(lambda x: )
-#  zones_df['zone_id'] = zones_df['zone_id'].apply(lambda row: mean_comp(dv, row))
-#  points_df = match_segment_id(points_test, segment_test)
 
-# def match_segment_id(pixel_df, poly_df):
-#     print('Matching pixels with their segment ID...')
+def match_segment_id(points_df, segment_df):
 
-#     ''' Parsing through the extracted points and matching
-#         each pixel with the segment ID of the segment
-#         that contains it. '''
+    st = time.time()
 
-#     #for i, row in enumerate(pixel_df.itertuples(), 0):
+    for i, row in enumerate(points_df.itertuples(), 0):
+        print(row)
+        p = points_df
+        point = points_df.at[i, 'geometry']
 
-#     def comp(pixel_df, poly_df):
-#         point = pixel_df.at[x, 'geometry']
 
-#         for j in range(len(poly_df)):
-#               poly = poly_df.iat[j, 0]
+        for j in range(len(segment_df)):
+              poly = segment_df.iat[j, 0]
+              print(poly)
 
-#               if poly.contains(point):
-#                   pixel_df.at[x, 'segment_id'] = poly_df.iat[j, 1]
-#               else:
-#                   pass
-
-#     pixel_df.apply(lambda x: comp(pixel_df, poly_df, x), axis=1)
-
-#     return pixel_df
-
-# def comp(pixel_df, poly_df, row):
-#     point = pixel_df['geometry'][ind]
-
-#     for j in range(len(poly_df)):
-#           poly = poly_df.iat[j, 0]
-
-#           if poly.contains(point):
-#               pixel_df.at[row, 'segment_id'] = poly_df.iat[j, 1]
-#           else:
-#               pass
-
-# points_df[3].apply(lambda row: comp(points_df, segment_df, row), axis=1)
+              if poly.contains(point):
+                  points_df.at[i, 'segment_id'] = segment_df.iat[j, 1]
+              else:
+                  pass
+    en=time.time()
+    print('pixels and segments matched in {} sec'.format(en-st))
+    return points_df
 
 points_df = seg.match_segment_id(points_df, segment_df)
 
-points_df = lc.sample_raster(points_df, 'data/swindon/masked.tif', bands=bands)
+st = time.time()
+
+pixels = points_df.geometry.values
+polys = segment_df
+
+# new_ind = range(0, len(polys))
+# polys = polys.reindex(new_ind)
+
+# def aggregate_values(series, agg_dict):
+
+#     lower_col = pd.Series(data=np.zeros(series.shape))
+#     for k, v in agg_dict.items():
+#         lower_col[series.isin(v)] = k
+
+#     return lower_col
+for pix in points_df.geometry:
+    for poly in polys.geometry:
+        if poly.contains(pix):
+            
+
+
+
+for i in range(0, len(pixels)):
+    poi = pixels[i]
+    for j in range(len(polys)):
+        poly = polys.geometry.iat[j]
+        if poly.contains(poi):
+            points_df.at[i, 'segment_id'] = segment_df.iat[j, 1]
+        else:
+            pass
+
+en=time.time()
+print('pixels and segments matched in {} sec'.format(en-st))
+
+
+
+
+
+
+
+
+
+
+points_df = lc.sample_raster(points_df, root_path + '/data/masked.tif', bands=bands)
  
 clean_df = lc.remove_outliers(points_df, bands=bands, indices=False)
 clean_df = lc.calc_indices(clean_df)
@@ -143,6 +186,7 @@ X_test = preprocessing.scale(X_test)
 
 preds = len(predictors);preds
 labs = len(list(clean_df[class_cols].unique()))
+vals = int(max(clean_df[class_cols])+1)
 
 input_num_units = preds
 hidden1_num_units = 200
@@ -151,32 +195,18 @@ hidden3_num_units = 200
 hidden4_num_units = 200
 output_num_units = labs
 
-model = Sequential([
-    Dense(output_dim=hidden1_num_units,
-          input_dim=input_num_units,
-          kernel_regularizer=l2(0.0001),
-          activation='relu'),
-    Dropout(0.2),
-    Dense(output_dim=hidden2_num_units,
-          input_dim=hidden1_num_units,
-          kernel_regularizer=l2(0.0001),
-          activation='relu'),
-    Dropout(0.2),
-    Dense(output_dim=hidden3_num_units,
-          input_dim=hidden2_num_units,
-          kernel_regularizer=l2(0.0001),
-          activation='relu'),
-    Dropout(0.1),
-    Dense(output_dim=hidden4_num_units,
-          input_dim=hidden3_num_units,
-          kernel_regularizer=l2(0.0001),
-          activation='relu'),
-    Dropout(0.1),
-    Dense(output_dim=(max(clean_df[class_cols])+1),
-          input_dim=hidden4_num_units, 
-          activation='softmax'),
- ])
-    
+model = Sequential()
+
+model.add(Dense(input_shape=(6,), output_dim=hidden1_num_units, activation='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(hidden2_num_units, activation='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(hidden3_num_units, activation='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(hidden4_num_units, activation='relu'))
+model.add(Dropout(0.1))
+model.add(Dense(vals, activation='softmax'))
+
 model.summary()
 
 sgd = optimizers.SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
@@ -221,3 +251,73 @@ print("Baseline Error: %.2f%%" % (100-score[1]*100))
 
 if __name__ == "__main__":
     seg
+
+
+
+# points_test = points_df.iloc[0:1000,:]
+# segment_test = segment_df.iloc[0:1000,:]
+#points_df['segment_id'] = points_df.iloc[:,-1].apply(lambda x: )
+#  zones_df['zone_id'] = zones_df['zone_id'].apply(lambda row: mean_comp(dv, row))
+#  points_df = match_segment_id(points_test, segment_test)
+
+# def match_segment_id(pixel_df, poly_df):
+#     print('Matching pixels with their segment ID...')
+
+#     ''' Parsing through the extracted points and matching
+#         each pixel with the segment ID of the segment
+#         that contains it. '''
+
+#     #for i, row in enumerate(pixel_df.itertuples(), 0):
+
+#     def comp(pixel_df, poly_df):
+#         point = pixel_df.at[x, 'geometry']
+
+#         for j in range(len(poly_df)):
+#               poly = poly_df.iat[j, 0]
+
+#               if poly.contains(point):
+#                   pixel_df.at[x, 'segment_id'] = poly_df.iat[j, 1]
+#               else:
+#                   pass
+
+#     pixel_df.apply(lambda x: comp(pixel_df, poly_df, x), axis=1)
+
+#     return pixel_df
+
+# def comp(pixel_df, poly_df, row):
+#     point = pixel_df['geometry'][ind]
+
+#     for j in range(len(poly_df)):
+#           poly = poly_df.iat[j, 0]
+
+#           if poly.contains(point):
+#               pixel_df.at[row, 'segment_id'] = poly_df.iat[j, 1]
+#           else:
+#               pass
+
+# points_df[3].apply(lambda row: comp(points_df, segment_df, row), axis=1)
+#([
+#     Dense(output_dim=hidden1_num_units,
+#           input_dim=input_num_units,
+#           kernel_regularizer=l2(0.0001),
+#           activation='relu'),
+#     Dropout(0.2),
+#     Dense(output_dim=hidden2_num_units,
+#           input_dim=hidden1_num_units,
+#           kernel_regularizer=l2(0.0001),
+#           activation='relu'),
+#     Dropout(0.2),
+#     Dense(output_dim=hidden3_num_units,
+#           input_dim=hidden2_num_units,
+#           kernel_regularizer=l2(0.0001),
+#           activation='relu'),
+#     Dropout(0.1),
+#     Dense(output_dim=hidden4_num_units,
+#           input_dim=hidden3_num_units,
+#           kernel_regularizer=l2(0.0001),
+#           activation='relu'),
+#     Dropout(0.1),
+#     Dense(output_dim=(max(clean_df[class_cols])+1),
+#           input_dim=hidden4_num_units, 
+#           activation='softmax'),
+#  ])
--- segment.py
+++ segment.py
@@ -1,31 +1,24 @@
-from numpy.random import seed
-seed(1)
-from tensorflow import set_random_seed
-set_random_seed(2)
+import os
 
 import geopandas as gpd
-import tensorflow as tf
 import numpy as np
 import pandas as pd
 import rasterio as rio
-import pylab as pl
 import matplotlib.pyplot as plt
-import seaborn as sns
-
-import land_classification as lc
 
 from rasterio import features
 from rasterstats import zonal_stats
-from fiona.crs import from_epsg
-from shapely.geometry import box as geobox
 from shapely.geometry import shape
 import json
 import geojson
+import time
 
 from skimage.segmentation import mark_boundaries
 from skimage.segmentation import felzenszwalb
 import tifffile as tiff
 import scipy.misc
+
+root_path = os.getcwd()
 
 def create_segment_polygon(tif):
     data = tif.read()
@@ -45,7 +38,7 @@ def create_segment_polygon(tif):
 def plot_segments(segments):
     print('Plotting segments over scene render...')
 
-    image_to_plot = rio.open('data/segment/masked_image_render.tif')
+    image_to_plot = rio.open(root_path + '/data/masked_image_render.tif')
     data = image_to_plot.read()
 
     plot_data = data[0:3]  # mark_boundaries needs [width, height, 3] format
@@ -66,7 +59,8 @@ def get_zones_and_dists(df):
     ''' Getting zonal stats for each segment and calculating 
         mean distributions. '''
 
-    segment_stats = zonal_stats(df, 'data/swindon/masked.tif',
+    st = time.time()
+    segment_stats = zonal_stats(df, root_path + '/data/masked.tif',
                                 stats = 'count min mean max median')
 
     zones_df = pd.DataFrame(segment_stats)
@@ -82,7 +76,8 @@ def get_zones_and_dists(df):
     for i in range(0, len(dists)):
         d = np.percentile(zone_means, dists[i])
         dist_values.append(d)
-
+    en=time.time()
+    print('zone stats in {} sec'.format(en-st))
     return zones_df, tuple(dist_values)
 
 def tag_zones(df, dv):
@@ -90,6 +85,7 @@ def tag_zones(df, dv):
 
     ''' Giving each segment a number between 1-5 based on their 
         similarity to the distribution percentiles. '''
+    st = time.time()
 
     for idx, row in df.iterrows():
         if (df.at[idx, 'mean']) < 1.2 * dv[0]:
@@ -108,7 +104,8 @@ def tag_zones(df, dv):
             df.at[idx, 'zone_id'] = 5
 
     df = df.dropna()
-
+    en=time.time()
+    print('zones tagged in {} sec'.format(en-st))
     return df
 
 def match_segment_id(pixel_df, poly_df):
@@ -117,6 +114,7 @@ def match_segment_id(pixel_df, poly_df):
     ''' Parsing through the extracted points and matching
         each pixel with the segment ID of the segment
         that contains it. '''
+    st = time.time()
 
     for i, row in enumerate(pixel_df.itertuples(), 0):
         point = pixel_df.at[i, 'geometry']
@@ -128,156 +126,8 @@ def match_segment_id(pixel_df, poly_df):
                   pixel_df.at[i, 'segment_id'] = poly_df.iat[j, 1]
               else:
                   pass
+    en=time.time()
+    print('pixels and segments matched in {} sec'.format(en-st))
+    return pixel_df    # takes ages, trying to make faster with apply
 
-    return pixel_df    # trying to make this faster
-
-# tif = rio.open('data/swindon/masked.tif')
-
-# segment_df, segments, shapes = create_segment_polygon(tif)
-# #zones_df, dist_values = get_zones_and_dists(segment_df)
-# zones_df = tag_zones(zones_df, dist_values)
-
-# segment_df['polygon id'] = zones_df['zone_id']
-
-# # segment_df = segment_df.dropna()
-
-# # Now need to add zone_id column to extracted pixels dataframe
-
-# pe = lc.PointExtractor(aoi)
-
-# points_df = pe.get_n(5000)
-
-# bands = ['B02', 'B03', 'B04', 'B08']
-
-# points_df = lc.sample_raster(points_df, 'data/Corine_S2_Proj_2.tif', bands=['labels'])
-# points_df['segment_id'] = np.nan
-# #points_df = match_segment_id(points_df, segment_df)
-# points_df = lc.sample_raster(points_df, 'data/swindon/masked.tif', bands=bands)
-
-# clean_df = lc.remove_outliers(points_df, bands=bands, indices=False)
-# clean_df = lc.calc_indices(clean_df)
-
-# class_cols = 'labels_1'
- 
-# predictors = ['B02_1','B03_1',
-#               'B04_1','B08_1',
-#               'ndwi',
-#               'segment_id']
-
-# clean_df = clean_df.drop(['savi', 'evi', 'ndvi'], axis=1)
-
-# X = clean_df[predictors]
-# X = X.values
-# y = clean_df[class_cols]
-# y = y.values
-
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-
-# X_train = preprocessing.scale(X_train)
-# X_test = preprocessing.scale(X_test)
-
-# preds = len(predictors)
-# labs = len(list(clean_df[class_cols].unique()))
-
-# input_num_units = preds
-# hidden1_num_units = 200
-# hidden2_num_units = 200
-# hidden3_num_units = 200
-# hidden4_num_units = 200
-# output_num_units = labs
-
-# model = Sequential([
-#     Dense(output_dim=hidden1_num_units,
-#           input_dim=input_num_units,
-#           kernel_regularizer=l2(0.0001),
-#           activation='relu'),
-#     Dropout(0.2),
-#     Dense(output_dim=hidden2_num_units,
-#           input_dim=hidden1_num_units,
-#           kernel_regularizer=l2(0.0001),
-#           activation='relu'),
-#     Dropout(0.2),
-#     Dense(output_dim=hidden3_num_units,
-#           input_dim=hidden2_num_units,
-#           kernel_regularizer=l2(0.0001),
-#           activation='relu'),
-#     Dropout(0.1),
-#     Dense(output_dim=hidden4_num_units,
-#           input_dim=hidden3_num_units,
-#           kernel_regularizer=l2(0.0001),
-#           activation='relu'),
-#     Dropout(0.1),
-#     Dense(output_dim=(max(clean_df[class_cols])+1),
-#           input_dim=hidden4_num_units, 
-#           activation='softmax'),
-#  ])
-    
-# model.summary()
-
-# sgd = optimizers.SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
-
-# # Compile model
-
-# model.compile(loss='sparse_categorical_crossentropy',
-#               optimizer='sgd',
-#               metrics=['accuracy'])
-
-# history=model.fit(X_train, 
-#           y_train,
-#           epochs=100, 
-#           batch_size=100, 
-#           validation_split = 0.2,
-#           verbose=1,
-#           )
-
-# plt.plot(history.history['acc'])
-# plt.plot(history.history['val_acc'])
-# plt.title('model accuracy')
-# plt.ylabel('accuracy')
-# plt.xlabel('epoch')
-# plt.legend(['train', 'test'], loc='lower right')
-# plt.show()
-
-# plt.plot(history.history['loss'])
-# plt.plot(history.history['val_loss'])
-# plt.title('model loss')
-# plt.ylabel('loss')
-# plt.xlabel('epoch')
-# plt.legend(['train', 'test'], loc='upper right')
-# plt.show()
-
-# # Model evaluation with test data set
-# # Prediction at test data set
-
-# y_pred = model.predict(X_test)
-# score = model.evaluate(X_test, y_test, batch_size=100, verbose=1)
-
-# # score_2 = model.score(X_test, y_test)
-# print(score)
-# print("Baseline Error: %.2f%%" % (100-score[1]*100))
-
-# Load and prepare the dataset to predict on
-
-# mask_src = tif
-# profile = mask_src.profile
-# data = mask_src.read(list(pl.arange(mask_src.count) + 1))
-# gdf = lc.create_raster_df(data, bands=bands)
-# gdf = lc.calc_indices(gdf)
-
-# cm = confusion_matrix(y_pred, y_test)
-# f, ax = pl.subplots(1, figsize=(20, 20))
-# sns.heatmap(ax=ax,
-#             data=cm, 
-#             annot=True, 
-#             fmt='g',
-#             cmap='pink',
-#             linewidths=0.5, 
-#             cbar=False)
-# ax.set_ylabel('Predicted')
-# ax.set_xlabel('True')
-# ax.set_title('Confusion matrix for Corine Level-2 Groups')
-# # pl.xticks(pl.arange(len(y_test.unique()))+0.5, plot_names, rotation=45)
-# # pl.yticks(pl.arange(len(y_test.unique()))+0.5, plot_names, rotation=45)
-# f.show()
-# f.savefig('outputs/cv_{}.png'.format('segmented_DNN'))
 
