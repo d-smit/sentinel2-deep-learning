93bca023976025d229d0e000c12ab421b8fb237f
David Smith <djksmith95@gmail.com>
2019-08-14 17:13:03 +0100

    split scene read, train and predict patch scripts. preds all 0s

--- patches.py
+++ patches.py
@@ -4,10 +4,12 @@ SEED = 12345
 tf.set_random_seed(SEED)
 np.random.seed(SEED)
 
+import os
 import geopandas as gpd
 from scipy import stats
 import matplotlib.pyplot as plt
 import json
+import pylab as pl
 import pandas as pd
 from fiona.crs import from_epsg
 from shapely.geometry import box as geobox
@@ -24,64 +26,71 @@ from sklearn.preprocessing import StandardScaler
 import collections
 import segment as seg
 from time import time
+from skimage.io import imread
+from keras.callbacks import ReduceLROnPlateau, EarlyStopping, ModelCheckpoint
 
 from sklearn.utils import class_weight
 
+root_path = os.getcwd()
+
 Segment = False
-Server = True
+# Server = True
 
-def prepare_scene():
-    aoi_geo = geobox(-2.29, 51.51, -1.71, 51.61)
-    aoi = gpd.GeoDataFrame([], geometry=[aoi_geo])
-    aoi.crs = from_epsg(4326)
-    aoi.to_file('data/aoi.geojson', driver='GeoJSON')
+path_to_model = root_path + '/models/'
+
+# def prepare_scene():
+#     aoi_geo = geobox(-2.29, 51.51, -1.71, 51.61)
+#     aoi = gpd.GeoDataFrame([], geometry=[aoi_geo])
+#     aoi.crs = from_epsg(4326)
+#     aoi.to_file('data/aoi.geojson', driver='GeoJSON')
     
-    with open('data/labels.json') as jf:
-        names = json.load(jf)
-    s2_band = 'S2A.SAFE'
+#     with open('data/labels.json') as jf:
+#         names = json.load(jf)
+#     s2_band = 'S2A.SAFE'
 
-    data, profile = lc.merge_bands(s2_band, res='10')
+#     data, profile = lc.merge_bands(s2_band, res='10')
     
-    lc.write_raster('data/merged.tif', data, profile)
-    lc.mask_raster(aoi, 'data/merged.tif', 'data/masked.tif')
+#     lc.write_raster('data/merged.tif', data, profile)
+#     lc.mask_raster(aoi, 'data/merged.tif', 'data/masked.tif')
 
-    return data, profile, names, aoi
+#     return data, profile, names, aoi
 
-data, profile, names, aoi = prepare_scene()
-# tif = rio.open('data/masked.tif', 'r')
+# data, profile, names, aoi = prepare_scene()
+# # tif = rio.open('data/masked.tif', 'r')
 
-def segment_scene(tif):
-    seg_df, segments, shapes = seg.create_segment_polygon(tif)
-    seg.plot_segments(segments)
-    zones_df, dists = seg.get_zones_and_dists(seg_df)
-    zones_df = seg.tag_zones(zones_df, dists)
-    seg_df['zone_id'] = zones_df['zone_id']
+# def segment_scene(tif):
+#     seg_df, segments, shapes = seg.create_segment_polygon(tif)
+#     seg.plot_segments(segments)
+#     zones_df, dists = seg.get_zones_and_dists(seg_df)
+#     zones_df = seg.tag_zones(zones_df, dists)
+#     seg_df['zone_id'] = zones_df['zone_id']
 
-    return seg_df
+#     return seg_df
 
-# seg_df = segment_scene(tif)
+# # seg_df = segment_scene(tif)
 
-pe = lc.PointExtractor(aoi)
-points_df = pe.get_n(50000)
+# pe = lc.PointExtractor(aoi)
+# points_df = pe.get_n(5000)
 
-def create_df(df, bands= ['B02', 'B03', 'B04', 'B08']):
+# def create_df(df, bands= ['B02', 'B03', 'B04', 'B08']):
 
-    points_df, values = sample_raster(df, 'data/Corine_S2_Proj_2.tif', bands=['labels'])
+#     points_df, values = sample_raster(df, 'data/Corine_S2_Proj_2.tif', bands=['labels'])
 
-    if Segment:
-        points_df = seg.match_segment_id(points_df, seg_df)
-        points_df = sample_raster(points_df, 'data/masked.tif', bands=bands)
-    else:
-        points_df, values = sample_raster(points_df, 'data/masked.tif', bands=bands)
+#     if Segment:
+#         points_df = seg.match_segment_id(points_df, seg_df)
+#         points_df = sample_raster(points_df, 'data/masked.tif', bands=bands)
+#     else:
+#         points_df, values = sample_raster(points_df, 'data/masked.tif', bands=bands)
 
-    # bands = ['B02_1', 'B03_1', 'B04_1','B08_1']
-    # points_df.to_file('points_shp_ex.geojson', driver='GeoJSON')
+#     # bands = ['B02_1', 'B03_1', 'B04_1','B08_1']
+#     points_df.to_file('points_shp_ex.geojson', driver='GeoJSON')
+#     np.savez_compressed('patch_arrays.npz', values)
 
-    print('pixel df stored')
+#     print('pixel df stored')
 
-    return points_df, values
+#     return points_df, values
 
-df, values = create_df(points_df)
+# df, values = create_df(points_df)
 
 def check_sim(df):
     for idx, row in df[label_cols].iterrows():
@@ -91,7 +100,11 @@ def check_sim(df):
     return df
 
 print('reading points df')
-# df = gpd.read_file('points_shp_ex.geojson')
+df = gpd.read_file('points_shp_ex.geojson')
+values = np.load('patch_arrays.npz')
+patches = values['arr_0']
+patches = patches.astype(float)
+patches = patches[:len(df)]
 
 def sort_cols(df):
     df = df.drop(['Lat', 'Lon', 'Val', 'geometry'], axis=1)
@@ -109,23 +122,21 @@ def sort_cols(df):
 
     return df
 
-df = sort_df_cols(df)
+df = sort_cols(df)
 
 counter = collections.Counter(df.labels_1.values)
 print('class dist: {}'.format(counter))
 
-def clean_scale(df):
+def clean_scale(df, patches):
 
-    df = df[(np.abs(stats.zscore(df)) < 1.5).all(axis=1)]
+    # df = df[(np.abs(stats.zscore(df)) < 1.5).all(axis=1)]
     # df = filter_low_counts(df, samples=100)
     df = onehot_targets(df)
-
     print('loading patch arrays and scaling...')
-    values = values[:len(df)]
-    df['patches'] = values
+
     arrays = []
     scalers = {}
-    for patch in df.patches.values:
+    for patch in patches:
         patch = patch.astype(float)
         for i in range(patch.shape[0]):
             scalers[i] = StandardScaler()
@@ -135,7 +146,7 @@ def clean_scale(df):
 
     return df, arrays
 
-df, arrays = clean_scale(df)
+df, arrays = clean_scale(df, patches)
 
 X = np.asarray(arrays)
 
@@ -143,6 +154,7 @@ class_cols = list(df['labels_1'].unique())
 y = df[class_cols]
 
 X_train, y_train = X, y
+
 # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
 def build_model(in_shape=(X[1].shape), out_shape=len(y.columns)):
@@ -163,7 +175,7 @@ def build_model(in_shape=(X[1].shape), out_shape=len(y.columns)):
      model.add(Dense(3200, activation='relu', kernel_initializer='he_uniform'))
      # model.add(Dropout(0.5))
      model.add(Dense(out_shape, activation='softmax'))
-     adam = optimizers.Adam(lr=0.01)
+     adam = optimizers.Adam(lr=0.00075)
 
      model.compile(optimizer=adam, loss='categorical_crossentropy', metrics=['accuracy'])
      return model
@@ -172,12 +184,34 @@ model = build_model()
 class_weightings = class_weight.compute_class_weight('balanced',
                                                       df.labels_1.unique(),
                                                       df.labels_1.values)
+
+reduce_lr = ReduceLROnPlateau(monitor='val_loss',
+                              factor=0.2,
+                              cooldown=1,
+                              patience=5,
+                              min_lr=0.0000001)
+
+earlystopper = EarlyStopping(monitor='val_acc',
+                              patience=10,
+                              mode='max')
+
+checkpointer = ModelCheckpoint(path_to_model + 'CNNPatch' +
+                                "_rgb_" +
+                                "{epoch:02d}-{val_acc:.3f}" +
+                                ".hdf5",
+                                monitor='val_acc',
+                                verbose=1,
+                                save_best_only=True,
+                                save_weights_only=False,
+                                mode='max')
+
 history=model.fit(X_train,
           y_train,
-          epochs=150,
+          epochs=250,
           batch_size=256,
-          validation_split = 0.2,
+          validation_split = 0.1,
           verbose=1,
+          callbacks=[reduce_lr, checkpointer, earlystopper],
           class_weight=class_weightings)
 
 plt.plot(history.history['acc'])
@@ -202,7 +236,7 @@ plt.show()
 if Server:
     plt.savefig('/home/strathclyde/seg/plots/loss_11x11.jpg')
 
-pred, proba, cm, algo = lc.classify(df, onehot=True)
+# pred, proba, cm, algo = lc.classify(df, onehot=True)
 
 # def build_model():
 
--- test_patches.py
+++ test_patches.py
@@ -1,51 +1,75 @@
 import os
-from skimage.io import imread
 import numpy as np
 from skimage.util import pad
+from skimage.io import imread
 from keras.models import load_model
 from tqdm import tqdm
+import land_classification as lc
 
 root_path = os.getcwd()
 
 Server = True
-Server = False
+# Server = False
 
 if Server:
-    path_to_image = root_path + '/DATA/bigearth/test/tci_medium.tif'
-    path_to_model = root_path + '/models/bigearth_rgb_02-0.488.hdf5'
+    path_to_image = root_path + '/data/masked.tif'
+    path_to_model = root_path + '/models/CNNPatch_rgb_01-0.449.hdf5'
 
 else:
-    path_to_image = root_path + '/data/test/tci_medium.tif'
-    path_to_model = root_path + '/data/models/bigearth_rgb_02-0.488.hdf5'
+    path_to_image = root_path + '/data/masked.tif'
+    path_to_model = root_path + '/models/CNNPatch_rgb_01-0.449.hdf5'
+
+scene = rio.open(path_to_image)
+profile = scene.profile
+data = scene.read(list(pl.arange(scene.count) + 1))
 
 model = load_model(path_to_model)
 
-_, input_rows, input_cols, channels = model.layers[0].input_shape
+_, patch_rows, patch_cols, bands = model.layers[0].input_shape
+
+patch_rows_rnd = (int((patch_rows)/2))
+patch_cols_rnd = (int((patch_cols)/2))
+
 _, output_classes = model.layers[-1].output_shape
 
 img = np.array(imread(path_to_image), dtype=float)
 
 _, unpadded_cols, _ = img.shape
 
-img = pad(img, ((input_cols, input_cols),
-                (input_rows, input_rows),
+img = pad(img, ((patch_cols, patch_cols),
+                (patch_rows, patch_rows),
                     (0, 0)), 'symmetric')
 
 num_rows, num_cols, _ = img.shape
 
 image_probs = np.zeros((num_rows, num_cols, output_classes))
 
-row_of_patches = np.zeros((unpadded_cols, input_rows, input_cols, channels))
+row_of_patches = np.zeros((unpadded_cols, patch_rows, patch_cols, bands))
 
-for row in tqdm(range(input_rows, num_rows-input_rows), desc="Processing..."):
+for row in tqdm(range(patch_rows, num_rows-patch_rows), desc="Processing..."):
 
-    for idx, col in enumerate(range(input_cols, num_cols-input_cols)):
+    for idx, col in enumerate(range(patch_cols, num_cols-patch_cols)):
 
-        row_of_patches[...] = img[row-int(input_rows/2):row+int(input_rows/2),
-                                  col-int(input_cols/2):col+int(input_cols/2),
+        row_of_patches[...] = img[row-patch_rows_rnd:(row+1)+patch_rows_rnd,
+                                  col-patch_cols_rnd:(col+1)+patch_cols_rnd,
                                   :]
 
     classified_row = model.predict(row_of_patches, batch_size=1, verbose=1)
-    image_probs[row, input_cols:num_cols-input_cols, :] = classified_row
+    image_probs[row, patch_cols:num_cols-patch_cols, :] = classified_row
+
+print('probs for image: {}'.format(image_probs))
+np.savez_compressed('scene_probs.npz', image_probs)
+
+probs = np.load('scene_probs.npz')
+probs = probs['arr_0']
+
+from scipy import stats
+stats.describe(probs)
+probs = np.argmax(probs, axis=1)
+lc.write_raster("outputs/lc_10m_test_proba.tif", probs, profile)
+
+
+
+
 
 
